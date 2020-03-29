48b5a60aae27e62b64773b4e617fc0b59d190f23
Pranay Agarwal <pranay12@connect.hku.hk>
2020-03-29 12:24:24 +0530

    Player walking animations

---
+++ Hacking Wong Project/Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 0.5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("speed", movement.sqrMagnitude);
+        if (movement.x < 0) animator.SetBool("isRight", false);
+        else if(movement.x >0) animator.SetBool("isRight", true);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
