edf9379f4b796d174c2c19b303bf6f4ac256b3cb
Pranay Agarwal <pranay12@connect.hku.hk>
2020-04-03 16:16:14 +0530

    Exploding Frog
    Minor porject clean up and boar as prefab with appropriate tags

---
+++ Hacking Wong Project/Assets/Scripts/Frog_Movement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Frog_Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody2D rb;
+    public float speed;
+    public Animator animator;
+    public Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        animator.SetFloat("horizontal", movement.x);
+        animator.SetFloat("vertical", movement.y);
+        animator.SetFloat("speed", movement.sqrMagnitude);
+        if (movement.x < 0) animator.SetBool("isRight", false);
+        else if (movement.x > 0) animator.SetBool("isRight", true);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            movement = Vector2.zero;
+            animator.SetTrigger("explode");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
