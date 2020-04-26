43c5ceff27d3730c54d66271e67a311b5f1c8d2f
Pranay Agarwal <47204059+pranay1208@users.noreply.github.com>
2020-04-26 13:56:25 +0530

    CS lab and Main Menu UI done (#2) (#3)
    Co-authored-by: Pranay Agarwal <47204059+pranay1208@users.noreply.github.com>
    
    Co-authored-by: Mahimaanand <47138963+Mahimaanand@users.noreply.github.com>

--- Hacking Wong Project/Assets/Scripts/Slot.cs
+++ Hacking Wong Project/Assets/Scripts/Slot.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class Slot : MonoBehaviour
 {
+  private Inventory inventory;
+  public int i;
+  private void Start(){
+    inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+  }
+  private void Update(){
+    if(transform.childCount <= 0){
+      inventory.isFull[i] = false;
+
+    }
+  }
   public void DropItem(){
       foreach (Transform child in transform){
           GameObject.Destroy(child.gameObject);
