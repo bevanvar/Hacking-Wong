5b05d000d74f43ddfbc342a82fc4f95eab5d826c
Mahimaanand <47138963+Mahimaanand@users.noreply.github.com>
2020-04-28 16:22:32 +0530

    Health potion switch up

--- Hacking Wong Project/Assets/Scripts/Pickup.cs
+++ Hacking Wong Project/Assets/Scripts/Pickup.cs
@@ -4,24 +4,31 @@ using UnityEngine;
 
 public class Pickup : MonoBehaviour
 {
-    private Inventory inventory;
-    public GameObject itemButton;
-    private void Start(){
-        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    HealthBar playerHealth;
+    Player_Movement healthValues;
 
+    public float healthBonus = 50f;
+
+    void Start(){
+        playerHealth = FindObjectOfType<HealthBar>();
+        healthValues = FindObjectOfType<Player_Movement>();
     }
-    void OnTriggerEnter2D(Collider2D other){
-        if(other.CompareTag("Player")){
-           for (int i = 0;i<inventory.slots.Length;i++){
-               if(inventory.isFull[i]==false){
-                   //Item can be added to inventory
-                   inventory.isFull[i] = true;
-                   Instantiate(itemButton, inventory.slots[i].transform,false);
-                   Destroy(gameObject);
-                   break;
 
-               }
-           } 
+    void Update(){
+        Debug.Log("ERROR");
+    }
+
+    void OnTriggerEnter2D(Collider2D col){
+        
+
+        if (col.CompareTag("Player")){ 
+            if(healthValues.currentHealth<healthValues.maxHealth){
+                Destroy(gameObject);
+                healthValues.currentHealth = healthValues.currentHealth + healthBonus;
+                playerHealth.SetHealth(healthValues.currentHealth);
         }
     }
+    }
+
+
 }
