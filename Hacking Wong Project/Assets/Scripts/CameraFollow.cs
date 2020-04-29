c5adba8235de8f9adcc409c828f51dc31576d394
Pranay Agarwal <pranay12@connect.hku.hk>
2020-04-29 17:47:09 +0530

    Colliders Scene 1 + Camera Movement

---
+++ Hacking Wong Project/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    Rigidbody2D target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.Find("Player").GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (!(target.position.x <= 0 || target.position.x >= 25.18))
+            transform.position = new Vector3(target.position.x, 0, -10);
+    }
+}
