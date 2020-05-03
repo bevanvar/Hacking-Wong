67c7a3fcaf7b21ed208f0946ec19b82d08404261
Pranay Agarwal <pranay12@connect.hku.hk>
2020-05-03 15:12:52 +0530

    Music Persistence
    Scene 1&2 have same music (Scene 1 music)
    Scene 3 has music of Scene 2

---
+++ Hacking Wong Project/Assets/MusicSource.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicSource : MonoBehaviour
+{
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+        if (GameObject.FindGameObjectsWithTag("Music").Length > 1)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+    }
+}
