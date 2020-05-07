f8199a3801a20a39ab7caaebecc9991c3867c085
bevanvar <bevanvar@connect.hku.hk>
2020-05-07 23:54:44 +0530

    ChimClip2

---
+++ Hacking Wong Project/Assets/Scripts/DeathClips.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathClips : MonoBehaviour
+{
+    public AudioSource bgmusic;
+    public AudioSource ChimClip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bgmusic.volume = 0.25f;
+        bgmusic.Play();
+        ChimClip.Play();
+        Invoke("Change", 3.8f);
+    }
+
+    void Change()
+    {
+        bgmusic.volume = 1f;
+    }
+
+}
